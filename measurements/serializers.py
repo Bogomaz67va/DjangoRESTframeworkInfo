3c8f2fdffb64de038ddb24a67bfe5a80037c24bf
bogomaz51 <Bogomaz51@gmail.com>
2021-07-01 15:07:30 +0300

    first commit

---
+++ measurements/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Project, Measurement
+
+
+class ProjectSerializer(ModelSerializer):
+    class Meta:
+        model = Project
+        fields = "__all__"
+
+
+class MeasurementSerializer(ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = "__all__"
